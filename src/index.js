32921dbaca2eb0a52bb8386bbbb0772395dc3106
Kostya9697 <alexandrov_sp131@mail.ru>
2023-01-11 20:27:12 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let N = n.toString().split('')
+    if(N[0] === '-'){
+        N.shift()
+    }
+    const newN = N.reverse().join('');
+    return newN
 }
+
